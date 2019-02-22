fd8126c149d85dea014731ed0637ba51250c84e9
joannayankova <joannayankova@gmail.com>
2019-02-22 07:54:21 -0800

    wpf chat

---
+++ ChatApi/Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.ServiceReference1;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //Step 1: Create an instance of the WCF proxy.
+            ServiceClient client = new ServiceClient();
+
+            string Message = Console.ReadLine();
+            var kyr =client.RecieveMessage(Message);
+
+            Console.WriteLine(kyr);
+
+
+            //Step 3: Closing the client gracefully closes the connection and cleans up resources.
+            client.Close();
+        }
+    }
+}
+
